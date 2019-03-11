d36b64f81b8f0ca81188f9138e945fde59c04d13
perblawiik <31038499+perblawiik@users.noreply.github.com>
2019-03-11 10:00:43 +0100

    Menu for scene navigation added

---
+++ ArucoUnity-2.0-alpha/ArucoUnity-2.0-alpha/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadCameraCalibrationScene ()
+    {
+        SceneManager.LoadScene("CalibrateCamera");
+    }
+
+    public void LoadTrackMarkersScene()
+    {
+        Debug.Log("TrackMarkers");
+        Application.Quit();
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("PerScene");
+    }
+}
