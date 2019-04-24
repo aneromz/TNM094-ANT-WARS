67f43fc650ceb0217b21b45a02c7bb0f3be76677
Adam Moren <adam.moren@hotmail.com>
2019-04-24 11:23:16 +0200

    myror med liv

--- Project/Assets/Scripts/AntBody.cs
+++ Project/Assets/Scripts/AntBody.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AntBody : MonoBehaviour
 {
+    public float health = 100f;
+    public Image healthBar;
+
+
     void OnMouseDown()
     {
-        GetComponentInParent<AgentControl>().CmdDestroyAnt();
+        TakeDamage();
+       // GetComponentInParent<AgentControl>().CmdDestroyAnt();
     }
+
+    public void TakeDamage()
+    {
+
+        health -= 40f;
+        healthBar.fillAmount = health / 100f;
+
+        if (health < 0.1f)
+        {
+            GetComponentInParent<AgentControl>().CmdDestroyAnt();
+        }
+
+       
+    }
+
 }
