79260ca279b17bff62eee628edd08e56138feee1
perblawiik <31038499+perblawiik@users.noreply.github.com>
2019-06-01 15:15:31 +0200

    Click On Spawn

---
+++ Project/Assets/Scripts/AntSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntSpawn : MonoBehaviour
+{
+
+    public string team;
+    private HeadUpDisplay hud;
+
+    private void Awake()
+    {
+        hud = FindObjectOfType<HeadUpDisplay>();
+    }
+
+    void OnMouseDown()
+    {
+        if (team == PlayerPrefs.GetString("team"))
+        {
+            if (tag == "Left")
+                hud.SpawnAntOnPosition1();
+            else if (tag == "Middle")
+                hud.SpawnAntOnPosition2();
+            else if (tag == "Right")
+                hud.SpawnAntOnPosition3();
+        }
+    }
+}
